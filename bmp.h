a5539d20239892e77aacab89804b576faccdb1a8
Vit Pavlik <pavlikvitek13@gmail.com>
2023-06-25 17:17:52 +0200

    bmp.h: change the library API

--- bmp.h
+++ bmp.h
@@ -9,17 +9,24 @@ typedef struct {
     uint8_t blue;
 } color_t;
 
+typedef struct {
+
+    /* image width */
+    uint32_t width;
+
+    /* image height */
+    uint32_t height;
+
+    /* array of pixels (bitmap) */
+    color_t *data;
+
+} image_t;
+
 /** 
- * creates specified bmp file with provided image data 
- * (single-dimensional array of color_t)
+ * creates specified bmp file with provided image struct
  * returns 0 on success, 1 otherwise 
  */
-int create_bmp(
-                    char filename[], 
-                    color_t data[], 
-                    uint16_t width, 
-                    uint16_t height
-);
+int create_bmp(char filename[], image_t image);
 
 
 
