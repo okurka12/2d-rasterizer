c793d2d6080379c924e573b1e98bd0df099f4db0
Vit Pavlik <pavlikvitek13@gmail.com>
2023-06-27 10:41:24 +0200

    bmp_save.c: use Windows BITMAPINFOHEADER instead

--- bmp_save.c
+++ bmp_save.c
@@ -1,9 +1,10 @@
 #include "bmp.h"
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 #define HEADER_SIZE 14
-#define DIB_HEADER_SIZE 12
+#define DIB_HEADER_SIZE 40
 
 typedef unsigned char byte;
 
@@ -18,12 +19,16 @@ uint8_t is_little_endian() {
 
 /* sets appropriate values to 14-long byte array `header` */
 void get_file_header(byte * header, uint16_t width, uint16_t height) {
-    uint32_t filesize = 14 + 12 + width * height * 3;
-    uint32_t offset = 14 + 12;  // where the pixel array starts
+    
+    /* file size */
+    uint32_t filesize = HEADER_SIZE + DIB_HEADER_SIZE + width * height * 3;
+    
+    /* where the pixel array starts */
+    uint32_t offset = HEADER_SIZE + DIB_HEADER_SIZE;
     
         header[0]  = 'B';                     // header field for BMP file
         header[1]  = 'M';
-        header[2]  = ((byte *)&filesize)[0];
+        header[2]  = ((byte *)&filesize)[0];  // file size
         header[3]  = ((byte *)&filesize)[1];
         header[4]  = ((byte *)&filesize)[2];
         header[5]  = ((byte *)&filesize)[3];
@@ -31,7 +36,7 @@ void get_file_header(byte * header, uint16_t width, uint16_t height) {
         header[7]  = 0;
         header[8]  = 0;                       // reserved for whatever idk
         header[9]  = 0;
-        header[10] = ((byte *)&offset)[0];
+        header[10] = ((byte *)&offset)[0];    // offset
         header[11] = ((byte *)&offset)[1];    
         header[12] = ((byte *)&offset)[2];
         header[13] = ((byte *)&offset)[3];
@@ -39,25 +44,31 @@ void get_file_header(byte * header, uint16_t width, uint16_t height) {
 }
 
 
-/* sets appropriate values to 12-byte long byte array `header`
-   (OS/2 1.x BITMAPCOREHEADER) */
-void get_dib_header(byte * header, uint16_t width, uint16_t height) {
-    uint32_t dib_header_size = 12;
+/* sets appropriate values to 40-byte long byte array `header`
+   (Windows BITMAPINFOHEADER) */
+void get_dib_header(byte * header, uint32_t width, uint32_t height) {
+    uint32_t dib_header_size = DIB_HEADER_SIZE;
     uint16_t color_planes_no = 1;
     uint16_t bits_per_pixel = 24;  // TrueColor
+    uint32_t compression_method = 0;  // None
+    uint32_t image_size = width * height * 3;  // size of the raw bitmap data
+    uint32_t horizontal_resolution = 2808;
+    uint32_t vertical_resolution = 2808;
+    uint32_t color_pallete_no = 0;  // 0 to default of 2 to the n
+    uint32_t important_color_no = 0;  // 0 - every color is important
 
-    header[0]  = ((byte *)&dib_header_size)[0];
-    header[1]  = ((byte *)&dib_header_size)[1];
-    header[2]  = ((byte *)&dib_header_size)[2];
-    header[3]  = ((byte *)&dib_header_size)[3];
-    header[4]  = ((byte *)&width)[0];
-    header[5]  = ((byte *)&width)[1];
-    header[6]  = ((byte *)&height)[0];
-    header[7]  = ((byte *)&height)[1];
-    header[8]  = ((byte *)&color_planes_no)[0];
-    header[9]  = ((byte *)&color_planes_no)[1];
-    header[10] = ((byte *)&bits_per_pixel)[0];
-    header[11] = ((byte *)&bits_per_pixel)[1];
+    memcpy(header +  0, &dib_header_size, 4);
+    memcpy(header +  4, &width, 4);
+    memcpy(header +  8, &height, 4);
+    memcpy(header + 12, &color_planes_no, 2);
+    memcpy(header + 14, &bits_per_pixel, 2);
+    memcpy(header + 16, &compression_method, 4);
+    memcpy(header + 20, &image_size, 4);
+    memcpy(header + 24, &horizontal_resolution, 4);
+    memcpy(header + 28, &vertical_resolution, 4);
+    memcpy(header + 32, &color_pallete_no, 4);
+    memcpy(header + 36, &important_color_no, 4);
+
 }
 
 
@@ -105,3 +116,36 @@ int bmp_save(char filename[], image_t *image) {
 
     return 0;   
 }
+
+/*
+
+DIB-HEADER spec (https://en.wikipedia.org/wiki/BMP_file_format)
+
+Offset-hex	Offset-dec Size-bytes Windows BITMAPINFOHEADER
+0E          14         4          the size of this header, in bytes (40)
+12          18         4          the bitmap width in pixels (signed integer)
+16          22         4          the bitmap height in pixels (signed integer)
+1A          26         2          the number of color planes (must be 1)
+1C          28         2          the number of bits per pixel, which is the color depth of the image. Typical values are 1, 4, 8, 16, 24 and 32.
+1E          30         4          the compression method being used. See the next table for a list of possible values
+22          34         4          the image size. This is the size of the raw bitmap data; a dummy 0 can be given for BI_RGB bitmaps.
+26          38         4          the horizontal resolution of the image. (pixel per metre, signed integer)
+2A          42         4          the vertical resolution of the image. (pixel per metre, signed integer)
+2E          46         4          the number of colors in the color palette, or 0 to default to 2n
+32          50         4          the number of important colors used, or 0 when every color is important; generally ignored
+
+compression-method value
+Value Identified-by  Compression-method               Comments
+0  BI_RGB	         none	                          Most common
+1  BI_RLE8	         RLE 8-bit/pixel	              Can be used only with 8-bit/pixel bitmaps
+2  BI_RLE4	         RLE 4-bit/pixel	              Can be used only with 4-bit/pixel bitmaps
+3  BI_BITFIELDS	     OS22XBITMAPHEADER: Huffman 1D	  BITMAPV2INFOHEADER: RGB bit field masks,
+                                                                     BITMAPV3INFOHEADER+: RGBA
+4  BI_JPEG	         OS22XBITMAPHEADER: RLE-24	      BITMAPV4INFOHEADER+: JPEG image for printing[14]
+5  BI_PNG		     BITMAPV4INFOHEADER+:             PNG image for printing[14]
+6  BI_ALPHABITFIELDS RGBA bit field masks	          only Windows CE 5.0 with .NET 4.0 or later
+11 BI_CMYK	         none	                          only Windows Metafile CMYK
+12 BI_CMYKRLE8	     RLE-8	                          only Windows Metafile CMYK
+13 BI_CMYKRLE4	     RLE-4	                          only Windows Metafile CMYK
+
+*/
