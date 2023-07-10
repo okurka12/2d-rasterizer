7d17077f116ff175194cb6e21e9802b51fa4917d
Vit Pavlik <pavlikvitek13@gmail.com>
2023-07-10 19:39:05 +0200

    main.c tryout of the macro functionality

--- main.c
+++ main.c
@@ -2,40 +2,60 @@
 #include "utils.h"
 #include <stdio.h>
 
+#define WIDTH 400
+#define HEIGHT 300
+
+const color_t red = {.red = 255};
+const color_t yel = {.red = 255, .green = 255};
+const color_t cyan = {.green = 255, .blue = 255};
+
+/* fills a square of `n` by `n` at `x`, `y` with color `col`*/
+void fill_sq(uint32_t x, uint32_t y, uint32_t n, color_t col, image_t *img) {
+    log("fill_sq zavolan");
+    for (uint32_t i = 0; i < n; i++) {
+        for (uint32_t j = 0; j < n; j++) {
+            logf("nastavuji pixel (%u, %u) na col", x + j, y + i);
+            pixel(x + j, y + i, img) = col;
+        }
+    }
+}
+
+/* generates image */
+void generate_image(image_t *img) {
+    fill_sq(10, 10, 10, red, img);
+    fill_sq(90, 5, 10, red, img);
+    fill_sq(300, 200, 10, red, img);
+}
+
 int main() {
 
+    /* timer thingies */
     timer_start(celkovy_cas);
     timer_start(generovani);
-
-    /* this is only a test obviously main.c is not going to look like this */
-    color_t red = {.red = 255};
-    color_t yel = {.red = 255, .green = 255};
-    // color_t cyan = {.green = 255, .blue = 255};
     
-    image_t *image = bmp_image_ctor(3333, 2222);
+    /* initialize empty image */
+    image_t *image = bmp_image_ctor(WIDTH, HEIGHT);
 
+    /* null check */
     if (image == NULL) {
         fprintf(stderr, "couldnt allocate or sth\n");
         return 1;
     }
 
-        for (unsigned int y = 0; y < image->height; y++) {
-        for (unsigned int x = 0; x < image->width; x++) {
-            if (x < image->width / 2 || y < image->height / 2) {
-                pixel(x, y, image) = yel;
-            }
-            else {
-                pixel(x, y, image) = red;
-            }
-            // image->data[image->width * y + x] = red;
-        }
-    }
+    /* generating of the picture itself */
+    generate_image(image);
 
+    /* timer thingies */
     timer_end(generovani, "generovani obrazku");
     timer_start(ukladani);
 
+    /* save image into file */
     int return_value = bmp_save("obrazecek_prvni.bmp", image); 
+    
+    /* free image */
     bmp_image_dtor(image);
+
+    /* check if saving into file was successful */
     if (return_value) {
         fprintf(stderr, "nepodarilo se udelat obrazek, abort\n");
         return 1;
@@ -43,6 +63,7 @@ int main() {
         printf("obrazek uspesne vytvoren\n");
     }
 
+    /* timer thingies */
     timer_end(ukladani, "ukladani");
     timer_end(celkovy_cas, "celkovy cas");
 
