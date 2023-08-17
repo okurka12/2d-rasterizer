5f43e236c033e72a7b8ad49f667cc6d7cb325f74
Vit Pavlik <pavlikvitek13@gmail.com>
2023-08-17 05:46:09 +0200

    bmp_image_ctor.c comment wording

--- bmp_image_ctor.c
+++ bmp_image_ctor.c
@@ -11,11 +11,11 @@ image_t *bmp_image_ctor(coord_t width, coord_t height) {
         return NULL;
     }
 
-    /* initialize struct */
+    /* initialize the image struct */
     output->width = width;
     output->height = height;
 
-    /* allocate */
+    /* allocate room for the bitmap */
     output->data = calloc(
         1, width*height*sizeof(color_t) + height*image_padding(width)
     );
