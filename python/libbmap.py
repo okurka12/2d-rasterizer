cd16b4b92e9f266194b45bd80c84606842ca3dec
Vit Pavlik <pavlikvitek13@gmail.com>
2023-08-17 05:45:04 +0200

    refactor + document libbmap.py

--- python/libbmap.py
+++ python/libbmap.py
@@ -2,8 +2,10 @@
 
 import ctypes as ctp
 
+SO_FILE = "./libbmap.so"  # path to the C dynamic library
+DEBUG = 1  # Do we want debug print statements?
+
 # logging
-DEBUG = 1
 if DEBUG:
     log = print
 else:
@@ -11,15 +13,22 @@ else:
         pass
 
 
-# the color type
 class Color:
+    """ an RGB color """
     def __init__(self, red: int, green: int, blue: int) -> None:
         self.red = red
         self.green = green
         self.blue = blue
 
+# common colors
+COLORS = {
+    "black": Color(0, 0, 0),
+    "white": Color(255, 255, 255)
+}
+
 
 class C_color_t(ctp.Structure):
+    """ C representation of class Color """
     _fields_ = [
         ("blue", ctp.c_uint8),
         ("green", ctp.c_uint8),
@@ -28,12 +37,14 @@ class C_color_t(ctp.Structure):
 
 
 class Point:
+    """ A 2D point """
     def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
 
 
 class C_point_t(ctp.Structure):
+    """ C representation of class Point """
     _fields_ = [
         ("x", ctp.c_uint32),
         ("y", ctp.c_uint32)
@@ -41,7 +52,6 @@ class C_point_t(ctp.Structure):
 
 
 # the C library API
-SO_FILE = "./libbmap.so"
 libbmap_c = ctp.CDLL(SO_FILE)
 libbmap_c.bmp_image_ctor.argtypes = ctp.c_uint32, ctp.c_uint32
 libbmap_c.bmp_image_ctor.restype  = ctp.c_void_p
@@ -55,23 +65,30 @@ libbmap_c.bmp_draw_rect.argtypes = (
 libbmap_c.bmp_save.argtypes = ctp.c_char_p, ctp.c_void_p
 libbmap_c.bmp_save.restype = ctp.c_int
 
-# the color black
-black = Color(0, 0, 0)
-
 class Image:
-    def __init__(self, x: int, y: int, color: Color = black) -> None:
-        """ constructor """
-        # TODO: color initialization
+    def __init__(self, x: int, y: int, color: Color = COLORS["black"]) -> None:
+        """ constructor of image `x` by `y` 
+            initialised to `color` (black by default) """
+        
+        # image construction
         self.image_p = libbmap_c.bmp_image_ctor(
             ctp.c_uint32(x), 
             ctp.c_uint32(y)
         )
+
+        # null check
+        if self.image_p == None:
+            raise RuntimeError("Couldn't allocate memory")
+        
         log("Constructed image at", hex(self.image_p))
+
+        # color initialisation
+        self.draw_rectangle(Point(0, 0), Point(x - 1, y - 1), color)
 
     def draw_rectangle(self, a: Point, b: Point, col: Color):
         """ Draws a rectangle from `a` to `b` of color `col` """
         log(f"drawing rectangle from ({a.x}, {a.y}) to ({b.x}, {b.y}) "
-             "to image at {self.image_p}")
+            f"to image at {hex(self.image_p)}")
         libbmap_c.bmp_draw_rect(
             C_point_t(a.x, a.y),
             C_point_t(b.x, b.y),
@@ -79,10 +96,12 @@ class Image:
             self.image_p
         )
 
-    def save_bmp(self, filename: str):
+    def save_bmp(self, filename: str) -> None:
         """ Saves the image as a .bmp file """
         log(f"saving \'{filename}\'")
-        libbmap_c.bmp_save(filename.encode("utf-8"), self.image_p)\
+        rcode = libbmap_c.bmp_save(filename.encode("utf-8"), self.image_p)
+        if rcode != 0:
+            raise RuntimeError(f"Couldnt save {filename}.")
 
     def __del__(self) -> None:
         """ destructor """
@@ -94,7 +113,7 @@ def main() -> None:
     """ This is a demo usage of the Image, Point and Color classes """
     a = Image(100, 200)
     a.draw_rectangle(Point(50, 50), Point(70, 80), Color(255, 0, 0))
-    a.save_bmp("obrazecek_druhy.bmp")
+    a.save_bmp("python_example.bmp")
 
 
 if __name__ == "__main__":
