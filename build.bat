16e283fa035f12eb2efe4d1031b09401bc114ef5
Vit Pavlik <pavlikvitek13@gmail.com>
2023-08-19 10:25:12 +0200

    build.bat clean + build.bat static

--- build.bat
+++ build.bat
@@ -2,10 +2,19 @@
 :: prerequisites: Visual Studio Build Tools (Desktop development with C++)
 :: this script is to be run in the developer command prompt
 
+:: usage
+:: .\build.bat                 builds dynamic library (+ example.exe)
+:: .\build.bat static          builds static library (+ example.exe)
+:: .\build.bat clean           removes all binaries (like `make clean`)
+
 :: works with
 :: Visual Studio build Tools 2022
 :: Microsoft (R) C/C++ Optimizing Compiler Version 19.35.32215 for x86
 :: Microsoft (R) Incremental Linker Version 14.35.32215.0
+
+:: See https://stackoverflow.com/a/51861571/15604091 to know if a .lib file
+:: is a static library or an import library for a dynamic library
+:: beware: a .lib file can be combination of both
 
 
 @set CC=cl
@@ -16,12 +25,18 @@
 :: modules
 @set modules=bmp_draw_rect bmp_image_ctor bmp_image_dtor bmp_save
 
-:: modules but with .obj extension
+:: .\build.bat clean
+@ if "%1" equ "clean" (
+    del *.o *.bin *.so *.a *.bmp *.obj *.exe *.dll *.exp *.lib
+    exit /b
+)
+
+:: create variable `modules` but with .obj extension
 @setlocal EnableDelayedExpansion
 @set objmodules=
 @ for %%m in (%modules%) do @set objmodules=!objmodules! %%m.obj
 
-:: modules but as export linker directives (`/EXPORT module`)
+:: create variable `modules` but as linker export directives (`/EXPORT module`)
 @setlocal EnableDelayedExpansion
 @set expmodules=
 @ for %%m in (%modules%) do @set expmodules=!expmodules! /EXPORT:%%m
@@ -29,9 +44,17 @@
 :: compile modules into object files
 @ for %%m in (%modules%) do %CC% %CFLAGS% /c %%m.c
 
-:: link modules into a library
-link /DLL /out:%DLL_FILENAME% %expmodules% %objmodules%
-:: TODO: implib a takovy picoviny
+:: .\build.bat static
+if "%1" equ "static" (
 
-:: compile example
+    :: create a static library (.lib)
+    lib /out:%LIB_FILENAME% %objmodules%
+
+) else (
+
+    :: create a dynamic library (.dll)
+    link /DLL /out:%DLL_FILENAME% %expmodules% %objmodules%
+)
+
+:: compile example.c and link 
 %CC% %CFLAGS% /Fe:example.exe example.c %LIB_FILENAME%
--- clean.bat
+++
@@ -1,1 +0,0 @@
-del *.o *.bin *.so *.a *.bmp *.obj *.exe *.dll *.exp *.lib
