2843e3e23cc42c653068676043c6d8fa10c4b86d
Vit Pavlik <pavlikvitek13@gmail.com>
2023-09-08 19:48:07 +0200

    bmp_draw_line.c: iterate by x

--- bmp_draw_line.c
+++ bmp_draw_line.c
@@ -1,3 +1,7 @@
+/**
+ * bmp_draw_line
+ * implements digital differential analyzer algorithm
+*/
 #include "bmp.h"
 #include "utils.h"
 
@@ -7,11 +11,11 @@
 #define FRAC_BITS 8
 
 void bmp_draw_line(point_t a, point_t b, color_t col, coord_t t, image_t *img) {
-    (void)a;
-    (void)b;
-    (void)col;
+
+    logf("%s called", __func__);
+
+    /* TODO: */
     (void)t;
-    (void)img;
 
     int64_t dx = (int64_t)b.x - a.x;
     int64_t dy = (int64_t)b.y - a.y;
@@ -23,6 +27,7 @@ void bmp_draw_line(point_t a, point_t b, color_t col, coord_t t, image_t *img) {
     if (dy > dx) {
 
         /* iterate by y */
+        /*--------------------------------------------------------------------*/
         
 
         /* go from a to b or from b to a? */
@@ -34,19 +39,48 @@ void bmp_draw_line(point_t a, point_t b, color_t col, coord_t t, image_t *img) {
             end = b;
         }
 
-        /* increment or decrement x? */
-        // signed int x_step = start.x < end.x ? -1 : 1
-
-        /* differential */
+        /* differential (upscaled) */
         int64_t k = (dx << FRAC_BITS) / dy;
 
+        /* initial x coordinate (upscaled) */
         coord_t x = start.x << FRAC_BITS;
+
+        /* iterate */
         for (coord_t y = start.y; y < start.y + dy; y++) {
-            logf("putting %u, %u", x >> FRAC_BITS, y);
+
+            /* put the pixel (first, downscale the x coordinate) */
             pixel(x >> FRAC_BITS, y, img) = col;
+
+            /* add the differential */
             x += k;
         }
 
 
+    } else {
+
+        /* iterate by x */
+        /*--------------------------------------------------------------------*/
+        
+        /* go from a to b or from b to a? */
+        if (b.x < a.x) {
+            start = b;
+            end = a;
+        } else {
+            start = a;
+            end = b;
+        }
+
+        /* differential */
+        int64_t k = (dy << FRAC_BITS) / dx;
+
+        /* initial y coordinate */
+        coord_t y = start.y << FRAC_BITS;
+        for (coord_t x = start.x; x < start.x + dx; x++) {
+            pixel(x, y >> FRAC_BITS, img) = col;
+            y += k;
+        }
+
     }
+
+
 }
